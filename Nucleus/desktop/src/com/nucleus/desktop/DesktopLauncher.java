8de80051c9971e97f0c5b414c574054464b3255a
HelgaGPataki <herman.carlstrom@gmail.com>
2016-04-14 11:39:23 +0200

    Project SHOULD be setup

---
+++ Nucleus/desktop/src/com/nucleus/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.nucleus.desktop;
+
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nucleus.Nucleus;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Nucleus(), config);
+	}
+}
