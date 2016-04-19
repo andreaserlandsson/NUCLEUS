cc2c2d8edde889f0892989b4f6b546341bf4f9a9
Erik <erikejan@hotmail.com>
2016-04-19 17:11:46 +0200

    Upgraded Nucleons. Added class diagram sketch

--- Nucleus/core/src/com/nucleus/Neutron.java
+++ Nucleus/core/src/com/nucleus/Neutron.java
@@ -3,5 +3,14 @@ package com.nucleus;
 /**
  * Created by erik on 19/04/16.
  */
-public class Neutron {
+public class Neutron extends Nucleon{
+
+    public Neutron(){
+        super();
+    }
+
+    public Neutron(int posX, int posY, int velX, int velY){
+        super(posX, posY, velX, velY);
+    }
+
 }
--- Nucleus/core/src/com/nucleus/Nucleon.java
+++ Nucleus/core/src/com/nucleus/Nucleon.java
@@ -9,9 +9,44 @@ public class Nucleon {
     private int posY;
     private int velX;
     private int velY;
-
+    private final int width = 0;   //Dummy value. Sizes will be used for collision detection purposes
+    private final int height = 0;  //and will be properly determined later.
+    private boolean isVisible = false;
 
     public Nucleon(){
 
+    }
+
+    public Nucleon(int posX, int posY, int velX, int velY){
+        this.posX=posX;
+        this.posY=posY;
+        this.velX=velX;
+        this.velY=velY;
+    }
+
+    public int getPosX(){
+        return posX;
+    }
+
+    public int getPosY(){
+        return posY;
+    }
+
+    public int getVelX(){
+        return velX;
+    }
+
+    public int getVelY(){
+        return velY;
+    }
+
+    public void setPosition(int x, int y){
+        posY = y;
+        posX = x;
+    }
+
+    public void setVelocity(int x, int y){
+        velX = x;
+        velY = y;
     }
 }
