049b2e61221f14046bf91230bfceed8cfb7d8e79
HelgaGPataki <herman.carlstrom@gmail.com>
2016-04-19 18:13:29 +0200

    modified file reader, possibly with lousy code..

--- Nucleus/core/src/com/nucleus/LevelParser.java
+++ Nucleus/core/src/com/nucleus/LevelParser.java
@@ -7,15 +7,15 @@ import java.util.Arrays;
 public class LevelParser {
 
     private static String levelString = ""; //temporary
-    private static int[] intList;   //obsolete?
+    //private static int[] intList;   //obsolete?
     public static void levelParse() {
-        splitLevelString(readFromFile());
+        splitLevelString(replaceCharacters(readFromFile()));
     }
 
+    // method to read from a file and return a single String.
     public static String readFromFile() {
-
         try {
-            Scanner sc = new Scanner(System.in);
+            Scanner sc;
             File file = new File("level_1.txt");    //temporary as we have no way to get a file
             sc = new Scanner(file);
             while (sc.hasNextLine()) {
@@ -23,13 +23,25 @@ public class LevelParser {
                 levelString = levelString + lineInput + "\n";
             }
             sc.close();
-
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         return levelString;
     }
 
+    // Strings are inmutable, so need to make a copy of the string to be able to change letters.
+    public static String replaceCharacters(String str) {
+        String temp = "";
+        for(int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                temp = temp + "\n";
+            } else {
+                temp = temp + str.charAt(i);
+            }
+        }
+        return temp;
+    }
+    //split a string where there is a new line
     public static void splitLevelString (String str) {
         String[] strings = str.split("\n");
         int[] numbers = new int[strings.length];
