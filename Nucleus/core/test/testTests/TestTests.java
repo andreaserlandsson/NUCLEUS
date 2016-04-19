047154c2da12f76e7cf91aba8f9e0e8922a65959
HelgaGPataki <herman.carlstrom@gmail.com>
2016-04-19 11:44:14 +0200

    tests for tests up and running

---
+++ Nucleus/core/test/testTests/TestTests.java
@@ -0,0 +1,16 @@
+package testTests;
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class TestTests {
+    @Test
+    public void thisAlwaysPasses() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored() {
+    }
+}
