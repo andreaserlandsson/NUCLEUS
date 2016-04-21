90d7ce6c1fb4c183180680e853aa4f99b1dc7848
HelgaGPataki <herman.carlstrom@gmail.com>
2016-04-21 14:30:07 +0200

    Levelbuilder to return Level, not void. start to implement list of nucleons to gun

--- Nucleus/core/src/com/nucleus/LevelBuilder.java
+++ Nucleus/core/src/com/nucleus/LevelBuilder.java
@@ -1,9 +1,11 @@
 package com.nucleus;
 
 
+import java.util.PriorityQueue;
+
 public class LevelBuilder{
 
-    public static void buildLevel(int level) {
+    public static Level buildLevel(int level) {
         int[] levelSpecs = LevelParser.levelParse(level);
         int levelNo = levelSpecs[0];
         int noOfGluonPoints = levelSpecs[1];
@@ -19,10 +21,14 @@ public class LevelBuilder{
             gluonPoints[j] = new GluonPoint(levelSpecs[i], levelSpecs[i + 1], levelSpecs[i + 2], levelSpecs[i + 3]);
             j++;
         }
+        PriorityQueue<Nucleon> pq = new PriorityQueue<Nucleon>(noOfProtons+noOfNeutrons);
+        for(int i = 0; i < noOfNeutrons; i++) {
+
+        }
+
+        NucleonGun nucleonGun = new NucleonGun();
         Molecule molecule = new Molecule(gluonPoints);
-
-
-
-
+        Level newLevel = new Level(levelNo, molecule);
+        return newLevel;
     }
 }
