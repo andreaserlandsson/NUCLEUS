73a58b38ecdee89e5764e3195a6386e6c50bcbe9
Erik <erikejan@hotmail.com>
2016-04-21 10:06:59 +0200

    Improved view

--- Nucleus/core/src/com/NucleusView.java
+++ Nucleus/core/src/com/NucleusView.java
@@ -3,6 +3,7 @@ package com;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.nucleus.LevelParser;
@@ -11,10 +12,14 @@ public class NucleusView extends ApplicationAdapter {
     SpriteBatch batch;
     Texture img;
 
+    private OrthographicCamera cam;
+
     @Override
     public void create () {
         batch = new SpriteBatch();
         img = new Texture("bg.png");
+        cam = new OrthographicCamera();
+        cam.setToOrtho(true, 136, Gdx.graphics.getHeight());
         LevelParser.levelParse(); //extremely temporary, delete!!
     }
 
