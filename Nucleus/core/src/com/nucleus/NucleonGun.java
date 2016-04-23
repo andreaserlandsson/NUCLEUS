77d03fcba3e9be9844f2645a131e55db615a6bc2
Erik <erikejan@hotmail.com>
2016-04-23 16:56:55 +0200

    Solved molecule and nucleongun's new interface dependencies. This was integrated in levelTest

--- Nucleus/core/src/com/nucleus/NucleonGun.java
+++ Nucleus/core/src/com/nucleus/NucleonGun.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * Created by erik on 19/04/16.
  */
 public class NucleonGun implements INucleonGun{
-    public NucleonGun(ArrayList<Nucleon> nucleons){
+    public NucleonGun(ArrayList<INucleon> nucleons){
 
     }
     /*
