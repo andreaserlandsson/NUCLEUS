e6ec000f40e02e9a7b3053e5eb3ccac61b5fbf1c
Erik <erikejan@hotmail.com>
2016-04-25 10:35:06 +0200

    Restructured starting sequence. LevelBuilder is bugged...

--- Nucleus/core/src/com/NucleusView.java
+++ Nucleus/core/src/com/NucleusView.java
@@ -6,10 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.nucleus.LevelParser;
-import com.nucleus.NucleusGame;
 
 public class NucleusView extends ApplicationAdapter {
     SpriteBatch batch;
@@ -20,26 +17,27 @@ public class NucleusView extends ApplicationAdapter {
 
     @Override
     public void create () {
-        batch = new SpriteBatch();
-        img = new Texture("bg.png");
-        cam = new OrthographicCamera();
 
-        cam.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        int screenWidth = Gdx.graphics.getWidth();
-        int screenHeight = Gdx.graphics.getHeight();
-
-        viewport = new ExtendViewport(screenWidth, screenHeight,
-                screenWidth, screenHeight, cam); // Supposed to keep aspect ratio but doesnt work..
-
-        int gameWidth = 288;
-        int gameHeight = 512;
-
-        NucleusGame game = new NucleusGame(gameWidth, gameHeight);
-
-        System.out.println("level 1  is; ");
-        LevelParser.levelParse(1); //extremely temporary, delete!!
-        System.out.println("level 2 is ;");
-        LevelParser.levelParse(2);
+//        batch = new SpriteBatch();
+//        img = new Texture("bg.png");
+//        cam = new OrthographicCamera();
+//
+//        cam.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+//        int screenWidth = Gdx.graphics.getWidth();
+//        int screenHeight = Gdx.graphics.getHeight();
+//
+//        viewport = new ExtendViewport(screenWidth, screenHeight,
+//                screenWidth, screenHeight, cam); // Supposed to keep aspect ratio but doesnt work..
+//
+//        int gameWidth = 288;
+//        int gameHeight = 512;
+//
+//        NucleusGame game = new NucleusGame(gameWidth, gameHeight);
+//
+//        System.out.println("level 1  is; ");
+//        LevelParser.levelParse(1); //extremely temporary, delete!!
+//        System.out.println("level 2 is ;");
+//        LevelParser.levelParse(2);
 
     }
 
@@ -47,14 +45,14 @@ public class NucleusView extends ApplicationAdapter {
     public void render () {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.end();
+        //batch.begin();
+        //batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        //batch.end();
     }
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height);
+        //viewport.update(width, height);
     }
 
     @Override
