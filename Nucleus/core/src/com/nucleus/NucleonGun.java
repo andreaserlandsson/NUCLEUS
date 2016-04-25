ea6b601c683d22b2b449e2bf8ec0428aef6be1de
fishur <sven-erik.erlandsson@hotmail.com>
2016-04-25 15:41:26 +0200

    complimented nucleonGun

--- Nucleus/core/src/com/nucleus/NucleonGun.java
+++ Nucleus/core/src/com/nucleus/NucleonGun.java
@@ -21,10 +21,45 @@ public class NucleonGun implements INucleonGun{
             INucleon a = nucleonList.get(arrayIndex);
             nucleonList.remove(arrayIndex);
             arrayIndex --;
-            return a; // returns
+
+            /*
+            config.width = 288;
+		    config.height = 512;
+             */
+            //sets the start position for the nucleon
+            int x = (int)Math.random()*3;
+            switch(x){
+                //längs med toppen
+                case 0:
+                    a.setPosition(288/(int)Math.random()*287+1,0); // We do not divide by zero
+                    break;
+
+                //längs med botten
+                case 1:
+                    a.setPosition(288/(int)Math.random()*287+1, 512); // We do not divide by zero
+                    break;
+
+                //lång med vänster långsida
+                case 2:
+                    a.setPosition(0,512/(int)Math.random()*511 +1); // We do not divide by zero
+                    break;
+
+                //längs med höger långsida
+                case 4:
+                    a.setPosition(288, 512/(int)Math.random()*511 +1); // We do not divide by zero
+                    break;
+
+                default:
+                    break;
+            }
+
+            //sets the next position for the nucleon ELLER?
+            a.setVelocity((int)(288/2*(Math.random()/10+0.01)), (int)(512/2*(Math.random()/10+0.01)));
+
+            return a;
         } else {
             System.out.println("nucleonList is empty");
-            return null; //
+            return null;
         }
     }
 
