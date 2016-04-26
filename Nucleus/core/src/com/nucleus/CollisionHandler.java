5f94d71858de705e487c9424b413e930369dca63
Miranda Bånnsgård <mirandabannsgard@dhcp-181241.eduroam.chalmers.se>
2016-04-26 13:01:19 +0200

    Method collision added to class CollisionHandler

--- Nucleus/core/src/com/nucleus/CollisionHandler.java
+++ Nucleus/core/src/com/nucleus/CollisionHandler.java
@@ -2,4 +2,28 @@ package com.nucleus;
 
 public class CollisionHandler {
 
+
+    public static boolean hasCollide(GluonPoint gluonPoint, Nucleon nucleon){
+
+        double distance;
+        int xPosGluonPoint = gluonPoint.getPosition().getX();
+        int yPosGluonPoint = gluonPoint.getPosition().getY();
+        int xPosNucleon = nucleon.getPosition().getX();
+        int yPosNucleon = nucleon.getPosition().getY();
+
+        distance = Math.sqrt((xPosGluonPoint-xPosNucleon)^2 + (yPosGluonPoint-yPosNucleon)^2);
+
+
+        if(distance < gluonPoint.getRadius() + nucleon.getRadius()){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    
+
+
+
 }
