a81779b71f963a75f60d225bcb23738beeb4dbbf
Miranda Bånnsgård <mirandabannsgard@dhcp-181241.eduroam.chalmers.se>
2016-04-26 14:16:09 +0200

    Minor changes in i CollisionHandle, I have no idea what I'm doing

--- Nucleus/core/src/com/nucleus/CollisionHandler.java
+++ Nucleus/core/src/com/nucleus/CollisionHandler.java
@@ -3,6 +3,7 @@ package com.nucleus;
 public class CollisionHandler {
 
 
+
     public static boolean collision(GluonPoint gluonPoint, Nucleon nucleon){
 
         double distance;
@@ -14,13 +15,21 @@ public class CollisionHandler {
         distance = Math.sqrt((xPosGluonPoint-xPosNucleon)^2 + (yPosGluonPoint-yPosNucleon)^2);
 
 
-        if(distance < gluonPoint.getRadius() + nucleon.getRadius()){
+        if((distance < gluonPoint.getRadius() + nucleon.getRadius())){
             return true;
         }else{
             return false;
         }
 
+        //TODO: A nucleon's collision with edge of screen? Does it just disappear?
+        //Need width and height from the gamescreen? Or something...
+
     }
+
+
+
+
+
 
 
 
