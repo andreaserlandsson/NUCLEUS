d989d0743e7f83cecd5e5c64c61491a4fa4186bf
Miranda Bånnsgård <bannsgard@gmail.com>
2016-04-29 20:54:39 +0200

    CollisionHandler using Vector methods

--- Nucleus/core/src/com/nucleus/CollisionHandler.java
+++ Nucleus/core/src/com/nucleus/CollisionHandler.java
@@ -2,31 +2,14 @@ package com.nucleus;
 
 public class CollisionHandler {
 
-
-
     public static boolean collision(IGluonPoint gluonPoint, INucleon nucleon){
 
-        double distance;
-        int xPosGluonPoint = gluonPoint.getPosition().getX();
-        int yPosGluonPoint = gluonPoint.getPosition().getY();
-        int xPosNucleon = nucleon.getPosition().getX();
-        int yPosNucleon = nucleon.getPosition().getY();
+        float distance = gluonPoint.getPosition().subtract(nucleon.getPosition()).abs();
 
-        distance = Math.sqrt( Math.pow((xPosGluonPoint-xPosNucleon),2) + Math.pow((yPosGluonPoint-yPosNucleon),2));
-
-
-        if((distance < gluonPoint.getRadius() + nucleon.getRadius())){
+        if((distance <= gluonPoint.getRadius() + nucleon.getRadius()))
             return true;
-        }else{
-            return false;
-        }
-
-        //TODO: A nucleon's collision with edge of screen? Does it just disappear?
-        //Need width and height from the gamescreen? Or something...
+        return false;
 
     }
-
-
-
 
 }
