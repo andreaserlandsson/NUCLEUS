88dda5e6856f0c6494d8e154f2a7b44b28df26f5
HelgaGPataki <herman.carlstrom@gmail.com>
2016-05-02 14:11:38 +0200

    new exception for levelparser

--- Nucleus/core/src/com/nucleus/LevelBuilder.java
+++ Nucleus/core/src/com/nucleus/LevelBuilder.java
@@ -8,7 +8,7 @@ public class LevelBuilder {
     // Builds level and from all data that is needed for that certain level.
     public static Level buildLevel(int width, int height, int levelNr) {
         LevelData levelData = LevelParser.levelParse(levelNr);
-
+        System.out.println("new exception here");
         // Fill a <INucleon> list with all protons and neutrons that it can pass onto the Gun.
         // Scrambles it before passing, so that there is a random order of protons/neutrons.
 
