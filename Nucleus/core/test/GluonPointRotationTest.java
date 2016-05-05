f0764735359fe302fa95e41d7c4ece84d35be51f
HelgaGPataki <herman.carlstrom@gmail.com>
2016-05-05 12:07:33 +0200

    merge confl in mockmol

--- Nucleus/core/test/GluonPointRotationTest.java
+++ Nucleus/core/test/GluonPointRotationTest.java
@@ -7,16 +7,17 @@ import static org.junit.Assert.assertTrue;
 
 public class GluonPointRotationTest {
 
+    //Variables that are used for all tests.
     IGluonPoint[] gluonPoints = new MockGluon[1];
     Molecule m = new Molecule(gluonPoints);
-
+    Vector centre = new Vector(0,0);
+    Vector gluonPos = new Vector(0,0);
     //Tests for rotation of a gluonPoint. First, ordinary tests to see that the method works.
 
     //Test for rotation 90 degrees right, and that the method correctly returns the new values.
     @Test
     public void rotate90DegreesRight(){
-        Vector centre = new Vector(0,0);
-        Vector gluonPos = new Vector(1,1);
+        gluonPos.setCoordinates(1,1);
         double angle = 90.0;
         Vector rotatedPosition = m.rotate(centre,gluonPos,angle);
         Vector result = new Vector(1,-1);
@@ -27,8 +28,7 @@ public class GluonPointRotationTest {
     //Test to do same rotation, but this time left.
     @Test
     public void rotate90DegreesLeft(){
-        Vector centre = new Vector(0,0);
-        Vector gluonPos = new Vector(1,1);
+        gluonPos.setCoordinates(1,1);
         double angle = -90.0;
         Vector rotatedPosition = m.rotate(centre,gluonPos,angle);
         Vector result = new Vector(-1,1);
@@ -39,8 +39,7 @@ public class GluonPointRotationTest {
     //Test to rotate back and forth, to see that no values are lost.
     @Test
     public void rotate90DegreesLeftThenRight(){
-        Vector centre = new Vector(0,0);
-        Vector gluonPos = new Vector(1,1);
+        gluonPos.setCoordinates(1,1);
         double leftRotation = -90.0;
         double rightRotation = 90;
         Vector leftRotated = m.rotate(centre,gluonPos,leftRotation);
@@ -54,8 +53,7 @@ public class GluonPointRotationTest {
     //as we no longer have fixed values of 1's and 0's.
     @Test
     public void negativeValuesAndRotationTest(){
-        Vector centre = new Vector(0,0);
-        Vector gluonPos = new Vector(-1,-1);
+        gluonPos.setCoordinates(-1,-1);
         double rotation = -45.0;
         Vector rotated = m.rotate(centre,gluonPos,rotation);
         Vector result = new Vector(-1.11E-16f,-1.414f);
@@ -66,12 +64,12 @@ public class GluonPointRotationTest {
     //Test for the most common rotation we will make, which is 1 degree.
     @Test
     public void oneDegreeRotation (){
-        Vector centre = new Vector(0,0);
-        Vector gluonPos = new Vector(1,1);
+        gluonPos.setCoordinates(1,1);
         double rotation = -1;
         Vector rotated = m.rotate(centre,gluonPos,rotation);
         Vector result = new Vector(0.9823f,1.017f);
         assertTrue(Math.abs(rotated.getX()-result.getX()) < 0.001);
         assertTrue(Math.abs(rotated.getY()-result.getY()) < 0.001);
     }
+
 }
