9b6fc78cec1528ef749de0f690298612bbedd964
Erik <erikejan@hotmail.com>
2016-05-06 17:37:32 +0200

    Test of Spritebatch.draw's rotation functionality. Not quite working...

--- Nucleus/core/src/com/nucleus/Views/MoleculeView.java
+++ Nucleus/core/src/com/nucleus/Views/MoleculeView.java
@@ -3,23 +3,34 @@ package com.nucleus.Views;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 /**
  * Created by erik on 28/04/16.
  */
 public class MoleculeView implements IView {
     private Texture moleculeTexture;
+    private TextureRegion moleculeTextureRegion;
     com.nucleus.Model.IMolecule molecule;
 
     //TODO: add support for loading different level textures
     public MoleculeView(com.nucleus.Model.IMolecule molecule){
         this.molecule = molecule;
         this.moleculeTexture = new Texture("1.png");
+        this.moleculeTextureRegion = new TextureRegion(moleculeTexture, moleculeTexture.getWidth(), moleculeTexture.getHeight());
     }
     public void render(SpriteBatch batch){
         batch.begin();
-        batch.draw(moleculeTexture, Gdx.graphics.getWidth() / 2 - moleculeTexture.getWidth() / 2,
-                Gdx.graphics.getHeight() / 2 - moleculeTexture.getHeight() / 2);
+        batch.draw(moleculeTextureRegion,
+                Gdx.graphics.getWidth() / 2 - moleculeTexture.getWidth() / 2,
+                Gdx.graphics.getHeight() / 2 - moleculeTexture.getHeight() / 2,
+                Gdx.graphics.getWidth() / 2 - moleculeTexture.getWidth() / 2,
+                Gdx.graphics.getHeight() / 2 - moleculeTexture.getHeight() / 2,
+                moleculeTexture.getWidth(),
+                moleculeTexture.getHeight(),
+                1.0f,
+                1.0f,
+                45.0f);
         batch.end();
     }
 }
