6bbd537be0eccbd6e46adc4115c0f02812312634
Hannes Lagerroth <hanneslagerroth@gmail.com>
2016-05-07 22:09:21 +0200

    Added separate "Screen" package for all screens

--- Nucleus/core/src/com/nucleus/ThirdParty/NucleusGame.java
+++ Nucleus/core/src/com/nucleus/ThirdParty/NucleusGame.java
@@ -11,7 +11,7 @@ public class NucleusGame extends Game {
         Gdx.app.log("Height", Integer.toString(Gdx.graphics.getHeight()));
         Gdx.app.log("Width", Integer.toString(Gdx.graphics.getWidth()));
         com.nucleus.Model.Level level = com.nucleus.Utils.LevelBuilder.buildLevel(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 1);
-        setScreen(new GameScreen(level));
+        setScreen(new com.nucleus.Screens.GameScreen(level));
         //TODO SKA DETA VARA EN INPUTADAPTER ELLER AV TYPEN INPUTCONTROLLER???
         Gdx.input.setInputProcessor(new NInputProcessor());
     }
