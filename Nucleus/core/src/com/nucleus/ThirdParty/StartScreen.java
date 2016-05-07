01b52caa2d2fc89b06e72bc370691c54d2e6e338
Hannes Lagerroth <hanneslagerroth@gmail.com>
2016-05-07 21:34:47 +0200

    Added non-functional and ugly buttons

--- Nucleus/core/src/com/nucleus/ThirdParty/StartScreen.java
+++ Nucleus/core/src/com/nucleus/ThirdParty/StartScreen.java
@@ -1,9 +1,20 @@
 package com.nucleus.ThirdParty;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.nucleus.Model.Level;
 import com.nucleus.Views.IView;
 import com.nucleus.Views.BackgroundView;
@@ -17,24 +28,56 @@ import java.util.List;
  * Created by Quaxi on 04/05/16.
  */
 public class StartScreen implements Screen {
-    private Level level;
-
-    private List<IView> views = new ArrayList<IView>();
-    //private OrthographicCamera cam;
 
     private SpriteBatch batch;
+    protected Stage stage;
+    private Viewport viewport;
+    private OrthographicCamera camera;
+    private TextureAtlas atlas;
+    protected Skin skin;
+    private Level level;
+    private List<IView> views = new ArrayList<IView>();
 
-    public StartScreen(){
+
+
+    public StartScreen()
+    {
         //this.cam = new OrthographicCamera(280, 560);
         //cam.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        views.add(new BackgroundView());
+
+       // views.add(new BackgroundView());
         batch = new SpriteBatch();
+        level = com.nucleus.Utils.LevelBuilder.buildLevel(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 1);
+
+
+        //atlas = new TextureAtlas("atlas.txt");
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera);
+        viewport.apply();
+
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+
+        stage = new Stage(viewport, batch);
+
+        //Stage should controll input:
+        Gdx.input.setInputProcessor(stage);
+
+
+
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(.1f, .12f, .16f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act();
+        stage.draw();
 
         for(IView view : views){
             view.render(batch);
@@ -44,11 +87,54 @@ public class StartScreen implements Screen {
     @Override
     public void resize(int width, int height){
         Gdx.app.log("GameScreen", "resizing");
+
+        viewport.update(width, height);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
     }
 
     @Override
     public void show(){
         Gdx.app.log("GameScreen", "showing");
+
+        //Create Table
+        Table mainTable = new Table();
+        //Set table to fill stage
+        mainTable.setFillParent(true);
+        //Set alignment of contents in the table.
+        mainTable.top();
+
+
+        //Create buttons
+        TextButton playButton = new TextButton("Play", skin);
+        TextButton optionsButton = new TextButton("Options", skin);
+        TextButton exitButton = new TextButton("Exit", skin);
+
+        //Add listeners to buttons
+        playButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.log("playButton", "clicked");
+                ((Game)Gdx.app.getApplicationListener()).setScreen(new GameScreen(level));
+            }
+        });
+        exitButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        //Add buttons to table
+        mainTable.add(playButton);
+        mainTable.row();
+        mainTable.add(optionsButton);
+        mainTable.row();
+        mainTable.add(exitButton);
+
+        //Add table to stage
+        stage.addActor(mainTable);
+
     }
 
     @Override
@@ -68,7 +154,9 @@ public class StartScreen implements Screen {
 
     @Override
     public void dispose() {
-        // Leave blank
+        skin.dispose();
+        atlas.dispose();
+
     }
 
 }
