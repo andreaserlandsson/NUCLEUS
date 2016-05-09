a8e52367e38ead01e793f5ef71edd6b678bacdd5
Hannes Lagerroth <hanneslagerroth@gmail.com>
2016-05-09 08:29:34 +0200

    Updated InputProcessor so the shit actually works

--- Nucleus/core/src/com/nucleus/ThirdParty/NucleusGame.java
+++ Nucleus/core/src/com/nucleus/ThirdParty/NucleusGame.java
@@ -12,8 +12,7 @@ public class NucleusGame extends Game {
         Gdx.app.log("Height", Integer.toString(Gdx.graphics.getHeight()));
         Gdx.app.log("Width", Integer.toString(Gdx.graphics.getWidth()));
         setScreen(new StartScreen());
-        //TODO SKA DETA VARA EN INPUTADAPTER ELLER AV TYPEN INPUTCONTROLLER???
-        Gdx.input.setInputProcessor(new NInputProcessor());
+       // Gdx.input.setInputProcessor(new NInputProcessor());
     }
 
     @Override
