2fd4a6d10cd7add35fb505d62bec432ead972fd2
Hannes Lagerroth <hanneslagerroth@gmail.com>
2016-05-11 16:35:20 +0200

    Trail now works, and are pretty good aligned.

--- Nucleus/core/src/com/nucleus/Viewable/NucleonViewable.java
+++ Nucleus/core/src/com/nucleus/Viewable/NucleonViewable.java
@@ -3,7 +3,6 @@ package com.nucleus.Viewable;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.g3d.Material;
 import com.nucleus.Model.INucleon;
 import com.nucleus.Model.Proton;
 import com.nucleus.Model.Vector;
@@ -16,15 +15,20 @@ import java.util.List;
 public class NucleonViewable implements IViewable {
 
     private List<INucleon> nucleons;
-    private Texture proton, neutron, smallTrailProton;
-    private TextureRegion smallTrail;
+    private Texture proton, neutron, protonLargeTrail, neutronLargeTrail;
+    private TextureRegion protonLargeTrailRegion, neutronLargeTrailRegion;
 
     public NucleonViewable(List<INucleon> nucleons){
 
         this.nucleons = nucleons;
 
-        smallTrailProton = new Texture("tailProton1.png");
-        smallTrail = new TextureRegion(smallTrailProton, smallTrailProton.getWidth(), smallTrailProton.getHeight());
+        protonLargeTrail = new Texture("tailProton1.png");
+        protonLargeTrail.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        protonLargeTrailRegion = new TextureRegion(protonLargeTrail, protonLargeTrail.getWidth(), protonLargeTrail.getHeight());
+
+        neutronLargeTrail = new Texture("tailNeutron1.png");
+        neutronLargeTrail.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        neutronLargeTrailRegion = new TextureRegion(neutronLargeTrail, neutronLargeTrail.getWidth(), neutronLargeTrail.getHeight());
 
         proton = new Texture("proton.png");
         proton.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
@@ -44,29 +48,43 @@ public class NucleonViewable implements IViewable {
 
     @Override
     public void render(SpriteBatch batch){
+
         batch.begin();
         batch.enableBlending();
+
         for(INucleon nucleon : nucleons){
             if (nucleon.getClass() == Proton.class){
-                float x = nucleon.getPosition().getX() - proton.getWidth() / 2;
-                float y = nucleon.getPosition().getY() - proton.getHeight() / 2;
-                batch.draw(proton, x, y);
-                batch.draw(smallTrail,
-                        x + proton.getWidth() /2,
-                        y + proton.getHeight() /2,
-                        proton.getWidth() / 2,
-                        proton.getHeight() / 2,
-                        smallTrailProton.getWidth(),
-                        smallTrailProton.getHeight(),
+                float x = nucleon.getPosition().getX();
+                float y = nucleon.getPosition().getY();
+                batch.draw(proton, x  - nucleon.getRadius(),y  - nucleon.getRadius());
+                batch.draw(protonLargeTrailRegion,
+                        //TODO fix 2px offset
+                        x-2,
+                        y + nucleon.getRadius(),
+                        nucleon.getRadius(),
+                        0,
+                        protonLargeTrail.getWidth(),
+                        protonLargeTrail.getHeight(),
                         1.0f,
                         1.0f,
                         vectorToRadians(nucleon.getVelocity()));
 
             }
             else{
-                float x = nucleon.getPosition().getX() - proton.getWidth() / 2;
-                float y = nucleon.getPosition().getY() - proton.getHeight() / 2;
-                batch.draw(neutron, x,  y);
+                float x = nucleon.getPosition().getX();
+                float y = nucleon.getPosition().getY();
+                batch.draw(neutron, x - nucleon.getRadius(),  y - nucleon.getRadius());
+                batch.draw(neutronLargeTrailRegion,
+                        //TODO fix 2px offset
+                        x-2,
+                        y + nucleon.getRadius(),
+                        nucleon.getRadius(),
+                        0,
+                        neutronLargeTrail.getWidth(),
+                        neutronLargeTrail.getHeight(),
+                        1.0f,
+                        1.0f,
+                        vectorToRadians(nucleon.getVelocity()));
 
             }
         }
