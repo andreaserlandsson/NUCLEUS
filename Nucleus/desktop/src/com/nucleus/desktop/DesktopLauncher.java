e1ac03c104f3364006d97b29e0b8d62b6233aa44
Hannes Lagerroth <hanneslagerroth@gmail.com>
2016-05-12 23:57:57 +0200

    Start screen now looks prettier

--- Nucleus/desktop/src/com/nucleus/desktop/DesktopLauncher.java
+++ Nucleus/desktop/src/com/nucleus/desktop/DesktopLauncher.java
@@ -9,8 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "NucleusGame";
-		config.width = 280;
-		config.height = 512;
+		config.width = 1280;
+		config.height = 720;
 		config.samples = 4;
 		new LwjglApplication(new NucleusGameCreator(), config);
 	}
