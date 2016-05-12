2861a25ec6e9f789c086227ea59756c9f1e715b3
Erik <erikejan@hotmail.com>
2016-05-12 12:16:44 +0200

    Adjusted screen size

--- Nucleus/desktop/src/com/nucleus/desktop/DesktopLauncher.java
+++ Nucleus/desktop/src/com/nucleus/desktop/DesktopLauncher.java
@@ -9,8 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "NucleusGame";
-		config.width = 1920;
-		config.height = 1024;
+		config.width = 280;
+		config.height = 512;
 		new LwjglApplication(new NucleusGameCreator(), config);
 	}
 }
