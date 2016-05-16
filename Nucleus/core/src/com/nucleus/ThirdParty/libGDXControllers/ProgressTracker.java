e5eee50fb504c92c79cc98f7f50eb2b7636058c5
Erik <erikejan@hotmail.com>
2016-05-16 11:40:55 +0200

    Refactoring using built-in Preferences function

--- Nucleus/core/src/com/nucleus/ThirdParty/libGDXControllers/ProgressTracker.java
+++ Nucleus/core/src/com/nucleus/ThirdParty/libGDXControllers/ProgressTracker.java
@@ -1,28 +1,25 @@
 package com.nucleus.ThirdParty.libGDXControllers;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.Preferences;
 import com.nucleus.Model.IProgressTracker;
 
 /**
  * Created by erik on 16/05/16.
  */
 public class ProgressTracker implements IProgressTracker{
-    FileHandle file;
+    Preferences prefs = Gdx.app.getPreferences("prefs");
 
     public ProgressTracker(){
-        this.file = Gdx.files.local("assets/levels/progress.txt");
     }
 
     public int readCompletedLevels(){
-        if(file.length()==0)
-            return 0;
-        String completedLvl = file.readString();
-        return Integer.parseInt(completedLvl);
+        int lvlProgress = prefs.getInteger("progress", 0);
+        return lvlProgress;
     }
 
     public void writeCompletedLevels(int levelNumber){
         if(readCompletedLevels()<levelNumber)
-            file.writeString(Integer.toString(levelNumber),false);
+            prefs.putInteger("progress", levelNumber);
     }
 }
