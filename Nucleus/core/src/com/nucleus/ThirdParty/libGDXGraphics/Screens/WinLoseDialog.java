5f43aedbd2b883c9019743c437f18ae8bbce49c0
fishur <sven-erik.erlandsson@hotmail.com>
2016-05-17 18:14:20 +0200

    did work on dialog classes

--- Nucleus/core/src/com/nucleus/ThirdParty/libGDXGraphics/Screens/WinLoseDialog.java
+++ Nucleus/core/src/com/nucleus/ThirdParty/libGDXGraphics/Screens/WinLoseDialog.java
@@ -1,6 +1,8 @@
 package com.nucleus.ThirdParty.libGDXGraphics.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 
 /**
  * Created by andreaserlandsson on 17/05/16.
