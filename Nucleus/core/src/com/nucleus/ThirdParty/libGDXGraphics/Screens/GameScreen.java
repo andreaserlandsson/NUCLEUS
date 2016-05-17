fa2545826b9e65b0d6adb4d92a55a47948ac687f
Miranda Bånnsgård <bannsgard@gmail.com>
2016-05-17 19:18:46 +0200

    Counter for protons and neutrons left almost finished, there are some minor esthetic flaws...

--- Nucleus/core/src/com/nucleus/ThirdParty/libGDXGraphics/Screens/GameScreen.java
+++ Nucleus/core/src/com/nucleus/ThirdParty/libGDXGraphics/Screens/GameScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.nucleus.Model.ILevel;
+import com.nucleus.Model.INucleonGun;
 import com.nucleus.ThirdParty.libGDXGraphics.Viewables.BackgroundViewable;
 import com.nucleus.ThirdParty.libGDXGraphics.Viewables.CountdownViewable;
 import com.nucleus.ThirdParty.libGDXGraphics.Viewables.IViewable;
@@ -33,7 +34,7 @@ public class GameScreen implements Screen {
         cam.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
         views.add(new BackgroundViewable());
-        views.add(new CountdownViewable());
+        views.add(new CountdownViewable(level.getNucleonGun()));
         views.add(new NucleonViewable(level.getAirborneNucleons()));
         views.add(new MoleculeViewable(levelNumber, level.getMolecule()));
 
