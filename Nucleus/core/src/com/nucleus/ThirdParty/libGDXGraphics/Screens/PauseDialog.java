6a65327631f8983204ee823b490bf0db29a5b590
fishur <sven-erik.erlandsson@hotmail.com>
2016-05-18 11:49:00 +0200

    did some work on pause feature

--- Nucleus/core/src/com/nucleus/ThirdParty/libGDXGraphics/Screens/PauseDialog.java
+++ Nucleus/core/src/com/nucleus/ThirdParty/libGDXGraphics/Screens/PauseDialog.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -27,6 +28,84 @@ public class PauseDialog extends ScreenAdapter {
     private ClickListener listener;
     private String[] buttons;
     private boolean won;
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage = new Stage());
+        skin = new Skin(Gdx.files.internal("menu/uiskin.json"));
+
+        new Dialog("Game Paused", skin) {
+
+            {
+                text("rly exit");
+                button("Continue palying", "con");
+                button("no", "glad you stay");
+            }
+
+            @Override
+            protected void result(final Object object) {
+                new Dialog("", skin) {
+
+                    {
+                        text(object.toString());
+                        button("OK");
+                    }
+
+                }.show(stage);
+            }
+
+        }.show(stage);
+    }
+
+    //	public static class ExitDialog extends Dialog {
+    //
+    //		public ExitDialog(String title, Skin skin, String windowStyleName) {
+    //			super(title, skin, windowStyleName);
+    //		}
+    //
+    //		public ExitDialog(String title, Skin skin) {
+    //			super(title, skin);
+    //		}
+    //
+    //		public ExitDialog(String title, WindowStyle windowStyle) {
+    //			super(title, windowStyle);
+    //		}
+    //
+    //		{
+    //			text("Do you really want to leave?");
+    //			button("Yes", "See you next time!");
+    //			button("No", "Enjoy your stay :)");
+    //		}
+    //
+    //		@Override
+    //		protected void result(Object object) {
+    //			System.out.println(object);
+    //		}
+    //
+    //	}
+
+    @Override
+    public void resize(int width, int height) {
+        //stage.setViewport(width, height);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        skin.dispose();
+    }
 
 
 }
