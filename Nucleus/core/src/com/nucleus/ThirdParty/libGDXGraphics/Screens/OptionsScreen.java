b7e6b86f75eb1f61c6d60a78fc63f6458de436ba
Erik <erikejan@hotmail.com>
2016-05-23 14:13:15 +0200

    Super preliminary options screen. Waiting for Hannes refactor.

---
+++ Nucleus/core/src/com/nucleus/ThirdParty/libGDXGraphics/Screens/OptionsScreen.java
@@ -0,0 +1,72 @@
+package com.nucleus.ThirdParty.libGDXGraphics.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.nucleus.ThirdParty.libGDXGraphics.Viewables.BackgroundViewable;
+import com.nucleus.ThirdParty.libGDXGraphics.Viewables.IViewable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by erik on 23/05/16.
+ */
+public class OptionsScreen implements Screen {
+
+    private SpriteBatch batch;
+    protected Stage stage;
+    private Viewport viewport;
+    private OrthographicCamera camera;
+    protected Skin skin;
+    private ClickListener listener;
+    private List<IViewable> views = new ArrayList<IViewable>();
+
+    public OptionsScreen(){
+        views.add(new BackgroundViewable());
+
+    }
+
+    @Override
+    public void render(float delta){
+
+    }
+
+    @Override
+    public void show(){
+        Gdx.app.log("OptionsScreen", "showing");
+        TextButton level1Button = new TextButton("Level 1", skin);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        Gdx.app.log("OptionsScreen", "resizing");
+
+    }
+
+    @Override
+    public void hide(){
+        Gdx.app.log("OptionsScreen", "hiding");
+    }
+
+    @Override
+    public void pause(){
+        Gdx.app.log("OptionsScreen", "pausing");
+    }
+
+    @Override
+    public void resume(){
+        Gdx.app.log("OptionsScreen", "resuming");
+    }
+
+    @Override
+    public void dispose() {
+        // Leave blank
+    }
+}
