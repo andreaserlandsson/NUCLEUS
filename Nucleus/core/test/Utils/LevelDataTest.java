7271442e4ee5be79a867e54604bb7b4c62c2b83a
HelgaGPataki <herman.carlstrom@gmail.com>
2016-05-25 14:11:27 +0200

    levelDataTest now working again, dont know why it was outcommented?

--- Nucleus/core/test/Utils/LevelDataTest.java
+++ Nucleus/core/test/Utils/LevelDataTest.java
@@ -1,5 +1,10 @@
 package Utils;
 
+import com.nucleus.Model.IGluonPoint;
+import com.nucleus.Model.LevelUtils.LevelData;
+
+import org.junit.Test;
+
 import static org.junit.Assert.assertTrue;
 
 
@@ -7,11 +12,11 @@ public class LevelDataTest {
 
     //Test to get all correct values from leveldata.
     //THIS SHOULD NOT BE DONE; WE SHOULD NOT USE GETTERS??
-    /*
+
     @Test
     public void levelDataIndexTest(){
         int[] mockLevel = new int[] {0,2,3,4,5,6,7,8,9,10,11,12,13,14};
-        LevelData levelData = new LevelData(mockLevel,280,512);
+        LevelData levelData = new LevelData(mockLevel,1,1);
         assertTrue(levelData.levelNo == mockLevel[levelData.levelNumberIndex]);
         assertTrue(levelData.noOfGluonPoints == mockLevel[levelData.gluonAmountIndex]);
         assertTrue(levelData.moleculeFilename == mockLevel[levelData.moleculeNameIndex]);
@@ -24,10 +29,12 @@ public class LevelDataTest {
     @Test
     public void oneGluonPointTest() {
         int[] mockLevel = new int[]{0, 1, 3, 4, 5, 6, 7, 8, 9, 10};
-        LevelData levelData = new LevelData(mockLevel,280,512);
+        LevelData levelData = new LevelData(mockLevel,1,1);
         //IGluonPoint[] mockGluons = levelData.getGluonPoints();
         IGluonPoint[] mockGluons = levelData.gluonPoints;
         assertTrue(mockGluons.length == mockLevel[levelData.gluonAmountIndex]);
+        System.out.println(mockGluons[0].getPosition().getX());
+        System.out.println(mockLevel[levelData.firstGluonIndex]);
         assertTrue(mockGluons[0].getPosition().getX() == mockLevel[levelData.firstGluonIndex]);
         assertTrue(mockGluons[0].getPosition().getY() == mockLevel[levelData.firstGluonIndex+ 1]);
         assertTrue(mockGluons[0].getProtonsNeeded() == mockLevel[levelData.firstGluonIndex+ 2]);
@@ -48,7 +55,7 @@ public class LevelDataTest {
     @Test
     public void twoGluonPointsTest(){
         int[] mockLevel = new int[] {0,2,3,4,5,6,7,8,9,10,11,12,13,14};
-        LevelData levelData = new LevelData(mockLevel,280,512);
+        LevelData levelData = new LevelData(mockLevel,1,1);
         IGluonPoint[] mockGluons = levelData.gluonPoints;
         assertTrue(mockGluons.length == mockLevel[levelData.gluonAmountIndex]);
         assertTrue(mockGluons[0].getPosition().getX() == mockLevel[levelData.firstGluonIndex]);
@@ -67,6 +74,4 @@ public class LevelDataTest {
         }
 
     }
-    */
-
 }
