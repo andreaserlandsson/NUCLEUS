4aa9382a16974966eb1e45c6ef4544a607fcaf12
fishur <sven-erik.erlandsson@hotmail.com>
2016-05-29 20:22:20 +0200

    changed some comments

--- Nucleus/core/src/com/nucleus/model/nucleusObservers/IObservable.java
+++ Nucleus/core/src/com/nucleus/model/nucleusObservers/IObservable.java
@@ -1,8 +1,5 @@
 package com.nucleus.model.nucleusObservers;
 
-/**
- * Created by erik on 26/05/16.
- */
 public interface IObservable<E> {
     void addObserver(IObserver<E> o);
     void removeObserver(IObserver<E> o);
--- Nucleus/core/src/com/nucleus/model/nucleusObservers/IObserver.java
+++ Nucleus/core/src/com/nucleus/model/nucleusObservers/IObserver.java
@@ -1,8 +1,5 @@
 package com.nucleus.model.nucleusObservers;
 
-/**
- * Created by erik on 26/05/16.
- */
 public interface IObserver<E> {
     void onObservation(IObservable<E> o, E arg);
 }
