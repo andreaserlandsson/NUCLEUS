021f8cebdb6d706ec8ea8d0d9b24c6906dfd58fa
fishur <sven-erik.erlandsson@hotmail.com>
2016-05-29 20:30:23 +0200

    changed some comments

--- Nucleus/core/src/com/nucleus/model/level/INucleonGun.java
+++ Nucleus/core/src/com/nucleus/model/level/INucleonGun.java
@@ -2,9 +2,6 @@ package com.nucleus.model.level;
 
 import com.nucleus.model.particles.INucleon;
 
-/**
- * Created by erik on 22/04/16.
- */
 public interface INucleonGun {
     INucleon shoot(); //should take a delta value, to time the guns shots
     boolean isEmpty();
